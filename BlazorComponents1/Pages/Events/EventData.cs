c7183053ed90227904bdad7325dad924f1ad7fb3
burgas275 <burgas275@gmail.com>
2020-08-09 04:09:51 +0300

    Add project files.

---
+++ BlazorComponents1/Pages/Events/EventData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorComponents1.Pages
+{
+    public class EventData
+    {
+        public int int1;
+        public string str1;
+    }
+}
